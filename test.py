db07d4e2ff8c05562185851aef0e52e6617618f4
Male Michael <malmike21@outlook.com>
2017-07-26 19:03:38 +0300

    Added an importation of base_test

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import unittest
+from tests import base_test
 
-suite = unittest.TestLoader()
+suite = unittest.TestLoader().loadTestsFromTestCase(base_test.BaseTest)
 
 unittest.TextTestRunner(verbosity=1).run(suite)
