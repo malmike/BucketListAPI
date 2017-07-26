490913134a787b53cb398820caf5fc7bb11e1d11
Male Michael <malmike21@outlook.com>
2017-07-26 22:07:58 +0300

    Modified base_test Added tests to check that the flask instance of the application is created for testing

--- tests/base_test.py
+++ tests/base_test.py
@@ -11,10 +11,19 @@ from myapp import create_app, db
 class BaseTest(TestCase):
 
     def create_app(self):
-        return create_app(environments['testing'])
+        self.app = create_app(environments['testing'])
+        return self.app
 
     def setUp(self):
         db.create_all()
+
+
+    def test_app_is_created(self):
+        self.assertTrue(self.app.config['DEBUG'])
+        self.assertTrue(
+            self.app.config['SQLALCHEMY_DATABASE_URI'] == "postgresql://root:root@localhost/test_db",
+            "Database configurations not setup"
+            )
 
     def tearDown(self):
         db.session.remove()
