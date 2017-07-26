9d613255bec4b22e9ccbc18e8fcecaf507a4a0ef
Male Michael <malmike21@outlook.com>
2017-07-26 20:19:11 +0300

    Modified base_test file to account for db creation and deletion Added method for creating the tables in the database and deleting the database while testing

--- tests/base_test.py
+++ tests/base_test.py
@@ -6,9 +6,16 @@ environment in which the tests can run
 from flask_testing import TestCase
 
 from instance import environments
-from myapp import create_app
+from myapp import create_app, db
 
 class BaseTest(TestCase):
 
     def create_app(self):
         return create_app(environments['testing'])
+
+    def setUp(self):
+        db.create_all()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
