0464cbaf153044c413e7221999a99502de5886eb
Male Michael <malmike21@outlook.com>
2017-07-27 19:55:11 +0300

    Modified UserTests Made it to use data from base_case for testing rather than it creating it's own data

--- tests/models/test_user.py
+++ tests/models/test_user.py
@@ -10,15 +10,14 @@ class UserTests(BaseCase):
     """
     Class contains tests for the user model
     """
-    
+
     def test_that_user_table_is_created(self):
         """
         Method checks that the user table is created
         """
-        user = User('test@test.com', 'test')
-        db.session.add(user)
-        db.session.commit()
+        user = User.query.filter_by(id=1).first()
         self.assertEqual(
-            1, user.id,
+            1,
+            user.id,
             "No data addded, so the table is not created"
         )
