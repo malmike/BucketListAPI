92855602d66783892a7e43b1e3df2fda775a5f8f
Male Michael <malmike21@outlook.com>
2017-07-27 10:10:48 +0300

    Modified test_user file Added test to check that the user table is created in the database

--- tests/models/test_user.py
+++ tests/models/test_user.py
@@ -10,4 +10,15 @@ class UserTests(BaseCase):
     """
     Class contains tests for the user model
     """
-    pass
+    
+    def test_that_user_table_is_created(self):
+        """
+        Method checks that the user table is created
+        """
+        user = User('test@test.com', 'test')
+        db.session.add(user)
+        db.session.commit()
+        self.assertEqual(
+            1, user.id,
+            "No data addded, so the table is not created"
+        )
