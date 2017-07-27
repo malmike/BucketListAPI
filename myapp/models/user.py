a7764038dd3f781456492d9a0c58e5eb893cbc73
Male Michael <malmike21@outlook.com>
2017-07-27 20:10:08 +0300

    Added arguments to represent the users details

--- myapp/models/user.py
+++ myapp/models/user.py
@@ -8,7 +8,10 @@ class User(db.Model):
     Class used as a representation of the user model
     """
     id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(50))
+    password = db.Column(db.String(50))
 
     def __init__(self, email, password):
-        pass
+        self.email = email
+        self.password = password
 
