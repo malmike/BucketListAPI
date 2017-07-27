c80b66ccee5fa06fc11906654aea2d94535da107
Male Michael <malmike21@outlook.com>
2017-07-27 22:08:00 +0300

    Added encryption to password using bycrpt

--- myapp/models/user.py
+++ myapp/models/user.py
@@ -1,19 +1,22 @@
 """
 Script contains the model for a user
 """
-from .. import db
+from .. import db, bcrypt, app
 
 class User(db.Model):
     """
     Class used as a representation of the user model
     """
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(50), unique=True)
-    password = db.Column(db.String(50))
+    email = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
 
     def __init__(self, email, password):
         self.email = email
-        self.password = password
+        self.password = bcrypt.generate_password_hash(
+            password, app.config.get('BCRYPT_LOG_ROUNDS')
+        )
+
 
     def __repr__(self):
         return '<UserEmail %r>' % self.email
