330dbabf2374a873ae0d2c5021216df106a5e498
Male Michael <malmike21@outlook.com>
2017-07-27 10:09:51 +0300

    Added test_user file It will contain tests for the user model

---
+++ tests/models/test_user.py
@@ -0,0 +1,13 @@
+"""
+Contains tests for the user model
+"""
+from tests.base_case import BaseCase
+
+from myapp import db
+from myapp.models.user import User
+
+class UserTests(BaseCase):
+    """
+    Class contains tests for the user model
+    """
+    pass
