05836abfe72c72cd6869bbef61a407a327f6937e
Male Michael <malmike21@outlook.com>
2017-07-27 14:48:55 +0300

    Added code climate test reporter id to test configurations

--- instance/config.py
+++ instance/config.py
@@ -8,7 +8,7 @@ class Config(object):
     """
     DEBUG = False
     TESTING = False
-    SECRET = 'KJI38US783KJW92MOY'
+    SECRET = 'KJI38US783KJW92MOYHSYE4837HNFYNE8347SH873UD384UFYHE'
     SQLALCHEMY_DATABASE_URI = "postgresql://localhost/bucketlist_api_dev"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
@@ -26,6 +26,7 @@ class TestConfig(Config):
     DEBUG = True
     TESTING = True
     SQLALCHEMY_DATABASE_URI = "postgresql://localhost/test_db"
+    CC_TEST_REPORTER_ID = "392653690efc9e65af92588e76d5d83f4c5660d20ee402b591b7df0035b60b7d"
 
 
 class ProductionConfig(Config):
