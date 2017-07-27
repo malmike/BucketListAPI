cc12e170f5fe8e5ded4d01e95ebb3134445d0d84
Male Michael <malmike21@outlook.com>
2017-07-27 22:04:33 +0300

    Added test to check that a user's password is encrypted

--- tests/models/test_user.py
+++ tests/models/test_user.py
@@ -1,12 +1,13 @@
 """
 Contains tests for the user model
 """
+from unittest import TestCase
 from tests.base_case import BaseCase
 
 from myapp import db
 from myapp.models.user import User
 
-class UserTests(BaseCase):
+class UserTests(BaseCase, TestCase):
     """
     Class contains tests for the user model
     """
@@ -32,3 +33,19 @@ class UserTests(BaseCase):
             "test@test.com",
             "User was not created"
         )
+
+    def test_that_the_password_is_encoded(self):
+        """
+        Method checks that password is encoded
+        """
+        user = User.query.filter_by(email="test@test.com").first()
+        self.assertNotEqual(user.password, 'test')
+
+    def test_that_password_are_encoded_differently(self):
+        """
+        Method that checks that the password is encoded
+        before storing in the database
+        """
+        user_test = User('test@test.com', 'test')
+        user = User.query.filter_by(email="test@test.com").first()
+        self.assertNotEqual(user.password, user_test.password)
