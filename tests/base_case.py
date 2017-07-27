5caafc43f27782324569fe5d93182aaffd3e9f1c
Male Michael <malmike21@outlook.com>
2017-07-27 08:29:02 +0300

    Added doc_strings to base_case file

--- tests/base_case.py
+++ tests/base_case.py
@@ -9,14 +9,27 @@ from instance import environments
 from myapp import create_app, db
 
 class BaseCase(TestCase):
-
+    """
+    This class is used to generate the default configurations for
+    all the test file
+    """
     def create_app(self):
-        self.app = create_app(environments['testing'])
-        return self.app
+        """
+        Creates a flask instance for testing
+        """
+        return create_app(environments['testing'])
 
     def setUp(self):
+        """
+        Sets up the default configurations, and in this case creates
+        the database to be used
+        """
         db.create_all()
 
     def tearDown(self):
+        """
+        Removes the default configurations, and in this case deletes the
+        database data and sessions in sqlalchemy after each test
+        """
         db.session.remove()
         db.drop_all()
