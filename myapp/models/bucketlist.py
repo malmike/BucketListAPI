bc116c22cb6cafc3b1dbb118ceacfba3c173843b
Male Michael <malmike21@outlook.com>
2017-07-28 23:28:53 +0300

    Modified bucketlist model class Added class variables with sqlalchemy table attributes to the bucketlist class

--- myapp/models/bucketlist.py
+++ myapp/models/bucketlist.py
@@ -1,14 +1,25 @@
 """
 Script contains the model for a bucketlist
 """
-from .. import db
+from myapp import db
+from datetime import datetime
+import pytz
 
 class BucketList(db.Model):
     """
     Class used as a representation of the user model
     """
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True, nullable=False)
+    date_created = db.Column(
+        db.DateTime,
+        default=datetime.now(tz=pytz.timezone('Africa/Kampala'))
+    )
+    date_modified = db.Column(
+        db.DateTime,
+        default=datetime.now(tz=pytz.timezone('Africa/Kampala')),
+        onupdate=datetime.now(tz=pytz.timezone('Africa/Kampala'))
+    )
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
-    def __init__(self, user_id, bucketlist_name):
-        pass
 
