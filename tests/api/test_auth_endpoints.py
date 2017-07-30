0c3d93ddfc90f30301280fcf88c1b8cae27edd18
Male Michael <malmike21@outlook.com>
2017-07-30 22:16:06 +0300

    Added test_auth_endpoints File contains tests for the authentication endpoints like user authentication, user registration, logout and user verification

---
+++ tests/api/test_auth_endpoints.py
@@ -0,0 +1,14 @@
+"""
+File contains tests for the authentication endpoints like user authentication,
+user registration, logout and user verification
+"""
+from unittest import TestCase
+
+from tests.base_case import BaseCase
+
+class AuthEndPointsTests(BaseCase, TestCase):
+    """
+    Class contains tests for the authentication endpoints like user authentication,
+    user registration, logout and user verification
+    """
+    
