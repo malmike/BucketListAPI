ec18757044b2f551d9aff8de4a17c173c9af686c
Male Michael <malmike21@outlook.com>
2017-07-30 18:53:33 +0300

    Added test file test_iser_view This will contain tests specific to the user's view file such as user registration and user authentication

---
+++ tests/api/views/test_user_view.py
@@ -0,0 +1,14 @@
+"""
+Contains tests for the user view that constitute those
+of user registaation and user authententication
+"""
+from unittest import TestCase
+
+from tests.base_case import BaseCase
+
+class UserViewTests(BaseCase, TestCase):
+    """
+    Class contains tests for the user views that constitute
+    those of user registration and user authentication
+    """
+    pass
