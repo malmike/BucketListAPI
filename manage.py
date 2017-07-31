b11b3ee4c6239c20de2e2118c023e87da7d20af6
Male Michael <malmike21@outlook.com>
2017-07-31 12:41:37 +0300

    Modified manage.py file

--- manage.py
+++ manage.py
@@ -2,24 +2,23 @@
 Script contains commands that can be called from the command line
 to a Manager instance, for the flask application
 """
-import os
-import unittest
-import coverage
-
+from os import getenv, path
+from sys import argv
+from unittest import TestLoader, TextTestRunner
+from coverage import Coverage
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
 from instance import ENVIRONMENTS
 from myapp import create_app, db
 
+ENV = getenv('BUCKETLIST_ENV') or 'development'
 
-ENV = os.getenv('BUCKETLIST_ENV') or 'development'
-app = create_app(ENVIRONMENTS.get(ENV))
-
-COV = coverage.Coverage(
-    branch = True,
-    include = 'myapp/*',
-    omit = [
+APP = create_app(ENVIRONMENTS.get(ENV))
+COV = Coverage(
+    branch=True,
+    include='myapp/*',
+    omit=[
         'tests/*',
         'instance/*',
         'manage.py'
@@ -27,37 +26,45 @@ COV = coverage.Coverage(
 )
 
 
-MIGRATE = Migrate(app, db)
-MANAGER = Manager(app)
+MIGRATE = Migrate(APP, db)
+MANAGER = Manager(APP)
 MANAGER.add_command('db', MigrateCommand)
 
 
 @MANAGER.command
 def testing():
-    tests = unittest.TestLoader().discover('tests', pattern='test*.py')
-    result = unittest.TextTestRunner(verbosity = 1).run(tests)
+    """
+    Calls and runs the tests
+    """
+    tests = TestLoader().discover('tests', pattern='test*.py')
+    result = TextTestRunner(verbosity = 1).run(tests)
     if result.wasSuccessful():
         return 0
     return 1
 
+
 @MANAGER.command
 def test_coverage():
+    """
+    Runs tests with coverage
+    """
     COV.start()
-    tests = unittest.TestLoader().discover('tests', pattern='test*.py')
-    result = unittest.TextTestRunner(verbosity = 1).run(tests)
+    tests = TestLoader().discover('tests', pattern='test*.py')
+    result = TextTestRunner(verbosity = 1).run(tests)
     if result.wasSuccessful():
         COV.use_cache(True)
         COV.stop()
         COV.save()
         print('Coverage report:')
         COV.report()
-        basedir = os.path.abspath(os.path.dirname(__file__))
-        covdir = os.path.join(basedir, 'htmlcov')
+        basedir = path.abspath(path.dirname(__file__))
+        covdir = path.join(basedir, 'htmlcov')
         COV.html_report(directory=covdir)
         print('HTML version: file://%s/index.html' % covdir)
         COV.xml_report()
         return 0
     return 1
+
 
 if __name__ == '__main__':
     MANAGER.run()
