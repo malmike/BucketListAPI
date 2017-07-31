02eefbbf58376c6199bc52ac9f18f84d04a3d889
Male Michael <malmike21@outlook.com>
2017-07-31 09:07:32 +0300

    Added resource class register user Class will contain methods that handle requests for registering a new user

--- myapp/api/auth_endpoints/views.py
+++ myapp/api/auth_endpoints/views.py
@@ -3,3 +3,14 @@ This file contains the endpoints for authentication
 i.e user registration, user authentication, user verification
 and logout
 """
+from flask_restplus import Resource
+
+class RegisterUser(Resource):
+    """
+    Class contains mthods that handle requests for registering a new user
+    """
+    def post(self):
+        """
+        Method receives post data used to register a new user
+        """
+        pass
