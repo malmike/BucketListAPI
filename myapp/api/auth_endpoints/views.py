baf4b2c9a8832907bde3cfef73a87b2f5ff15d57
Male Michael <malmike21@outlook.com>
2017-08-02 11:00:07 +0300

    Added method __validate_email It will validate the emails passed to the application during registration

--- myapp/api/auth_endpoints/views.py
+++ myapp/api/auth_endpoints/views.py
@@ -3,14 +3,23 @@ This file contains the endpoints for authentication
 i.e user registration, user authentication, user verification
 and logout
 """
-from flask_restplus import Resource
+from flask_restplus import Resource, abort
+from flask import request
+from re import search
+from myapp.models.user import User
 
 class RegisterUser(Resource):
     """
     Class contains mthods that handle requests for registering a new user
     """
-    def post(self):
+    
+
+    @staticmethod
+    def __validate_email(email):
         """
-        Method receives post data used to register a new user
+        Method validates that the email passed is valid
+        regular expression used is got from http://emailregex.com
         """
-        pass
+        email_re = r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)"
+        return True if search(email_re, email) else False
+
