2568b9ff8919506711adcf1b5e2784566b4fc9c8
Male Michael <malmike21@outlook.com>
2017-08-02 10:46:37 +0300

    Added test for user registration

--- tests/api/test_auth_endpoints.py
+++ tests/api/test_auth_endpoints.py
@@ -3,6 +3,7 @@ File contains tests for the authentication endpoints like user authentication,
 user registration, logout and user verification
 """
 from unittest import TestCase
+import json
 
 from tests.base_case import BaseCase
 
@@ -11,4 +12,24 @@ class AuthEndPointsTests(BaseCase, TestCase):
     Class contains tests for the authentication endpoints like user authentication,
     user registration, logout and user verification
     """
-    
+    def test_registration(self):
+        """
+        This method tests the user registration method
+        """
+        response = self.register_user("test@testing.com")
+        result = json.loads(response.data)
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(result['message'], 'Successfully Registered')
+
+
+    def register_user(self, email):
+        """
+        Method is used to carry out user registration for testing
+        """
+        _pword = "test"
+        return self.client.post(
+            '/api/v1/auth/register',
+            data=json.dumps({"email": email, "password": _pword}),
+            content_type="application/json",
+            follow_redirects=True
+        )
