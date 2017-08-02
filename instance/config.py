12087f97f59331af9a449e084ac2f07bccee0bbb
Male Michael <malmike21@outlook.com>
2017-08-02 10:55:32 +0300

    Modified config script Added variable that's used to state authentication token duration

--- instance/config.py
+++ instance/config.py
@@ -13,7 +13,9 @@ class Config(object):
     BCRYPT_LOG_ROUNDS = 13
     __secret_key = 'uMhwMM2q9L2Cg6kgTv2PFz7AbAUOsmMHuZCcwO0A7j6AdtzHuxAf9ctIUBZ4T0ea'
     SECRET_KEY = os.getenv('SECRET_KEY') or __secret_key
-    SQLALCHEMY_DATABASE_URI = "postgresql://localhost/bucketlist_api_dev"
+    AUTH_TOKEN_DURATION = os.getenv('TOKEN_DURATION') or 300
+    __dbname = "postgresql://localhost/bucketlist_api_dev"
+    SQLALCHEMY_DATABASE_URI = os.getenv('DB_NAME') or __dbname
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class DevelopmentConfig(Config):
