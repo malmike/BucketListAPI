4c7e4ceebb0bc16648031205f2b2a24ce68045b5
Male Michael <malmike21@outlook.com>
2017-08-06 01:05:13 +0300

    Added test for verify token method in utilities

--- tests/utilities/test_utilities.py
+++ tests/utilities/test_utilities.py
@@ -2,9 +2,11 @@
 Contains tests for the api utilities
 """
 from unittest import TestCase
+import json
 
 from tests.base_case import BaseCase
-from myapp.utilities.Utilities import validate_email
+from myapp.utilities.Utilities import validate_email, verify_token
+
 
 class UserTests(BaseCase, TestCase):
     """
@@ -24,3 +26,17 @@ class UserTests(BaseCase, TestCase):
         returns false
         """
         self.assertFalse(validate_email('testtests.com'))
+
+
+    def test_validate_token(self):
+        """
+        Tests the verify_token method in the utilities file
+        """
+        path = '/api/v1/auth/login'
+        response = self.post_user_data(path=path, email='test@test.com')
+        result = json.loads(response.data)
+        self.assertTrue(result['auth_token'])
+        self.assertTrue(
+            verify_token(result['auth_token'])
+        )
+
