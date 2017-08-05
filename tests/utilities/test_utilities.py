c4db1695b98858fd233d1f2f8ea4e21424ade89f
Male Michael <malmike21@outlook.com>
2017-08-05 21:57:21 +0300

    Added methods for testing  validate_email method

--- tests/utilities/test_utilities.py
+++ tests/utilities/test_utilities.py
@@ -10,4 +10,17 @@ class UserTests(BaseCase, TestCase):
     """
     Class contains tests for the api utilities
     """
-    pass
+    def test_valid_email(self):
+        """
+        Tests that when a vaild email is passed, validate_email method
+        returns true
+        """
+        self.assertTrue(validate_email('test@test.com'))
+
+
+    def test_invalid_email(self):
+        """
+        Ensures that when one enters an invalid email, validate_email method
+        returns false
+        """
+        self.assertFalse(validate_email('testtests.com'))
