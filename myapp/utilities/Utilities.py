ffb1fe3dafc048f9c37a1aabdccb5497d40071b6
Male Michael <malmike21@outlook.com>
2017-08-05 22:35:48 +0300

    Added method validate_email to utilities script

--- myapp/utilities/Utilities.py
+++ myapp/utilities/Utilities.py
@@ -2,4 +2,13 @@
 This script contatins utilities for the api i.e. general computations
 for the API like validate email
 """
-pass
+from re import search
+
+
+def validate_email(email):
+    """
+    Method validates that the email passed is valid
+    regular expression used is got from http://emailregex.com
+    """
+    email_re = r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)"
+    return True if search(email_re, email) else False
