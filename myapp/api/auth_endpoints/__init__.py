c73f6cb7865c1d63588ab8cead00dc3f51a2ed08
Male Michael <malmike21@outlook.com>
2017-08-05 15:26:09 +0300

    Changed restplus Api to use add_namespace instead of add_resource

--- myapp/api/auth_endpoints/__init__.py
+++ myapp/api/auth_endpoints/__init__.py
@@ -3,11 +3,11 @@ Initializes the auth endpoint module with blueprints
 """
 from flask import Blueprint
 from flask_restplus import Api
-from myapp.api.auth_endpoints.views import RegisterUser
+from .views import auth_api
 
 auth_blueprint = Blueprint('auth', __name__)
 api_v1 = Api(auth_blueprint)
 
 # Set up paths for the auth endpoints
 ##User registration path
-api_v1.add_resource(RegisterUser, '/auth/register', endpoint='register_endpoint')
+api_v1.add_namespace(auth_api)
