d7bccf65dc14c0856a611f6f31f32a46aaa8f675
Male Michael <malmike21@outlook.com>
2017-08-06 01:05:56 +0300

    Added method verify_token in Utilities

--- myapp/utilities/Utilities.py
+++ myapp/utilities/Utilities.py
@@ -3,6 +3,10 @@ This script contatins utilities for the api i.e. general computations
 for the API like validate email
 """
 from re import search
+from flask import g
+from flask_httpauth import HTTPTokenAuth
+
+auth = HTTPTokenAuth(scheme='Token')
 
 
 def validate_email(email):
@@ -12,3 +16,10 @@ def validate_email(email):
     """
     email_re = r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)"
     return True if search(email_re, email) else False
+
+
+@auth.verify_token
+def verify_token(token):
+    if g.current_user.verify_authentication_token(token):
+        return True
+    return False
