4075f4471eb60231a5c77e7e3d0d49594000d3db
Male Michael <malmike21@outlook.com>
2017-08-05 21:55:35 +0300

    Created class for testing the methods in the utilities file

---
+++ tests/utilities/test_utilities.py
@@ -0,0 +1,13 @@
+"""
+Contains tests for the api utilities
+"""
+from unittest import TestCase
+
+from tests.base_case import BaseCase
+from myapp.utilities.Utilities import validate_email
+
+class UserTests(BaseCase, TestCase):
+    """
+    Class contains tests for the api utilities
+    """
+    pass
