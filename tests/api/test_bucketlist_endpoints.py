653c0e0fbd62a8f3050273a251c6205620291ce3
Male Michael <malmike21@outlook.com>
2017-08-06 09:40:37 +0300

    Created test class for bucketlist endpoints

---
+++ tests/api/test_bucketlist_endpoints.py
@@ -0,0 +1,13 @@
+"""
+File contains tests for the bucketlist endpoints
+"""
+from unittest import TestCase
+import json
+
+from tests.base_case import BaseCase
+
+class BucketlistEndPointsTests(BaseCase, TestCase):
+    """
+    Class contains tests for the bucketlist endpoints
+    """
+    pass
