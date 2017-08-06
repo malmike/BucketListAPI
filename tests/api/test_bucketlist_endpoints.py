96e6280456d0303f2cdb7890c5e0b3c6ee6d12de
Male Michael <malmike21@outlook.com>
2017-08-06 12:07:03 +0300

    Added method to test end point for adding bucketlist

--- tests/api/test_bucketlist_endpoints.py
+++ tests/api/test_bucketlist_endpoints.py
@@ -12,6 +12,23 @@ class BucketlistEndPointsTests(BaseCase, TestCase):
     """
     Class contains tests for the bucketlist endpoints
     """
+    def test_add_bucketlist(self):
+        """
+        Method tests that the endpoint meant to add a bucketlist actually
+        adds a bucketlist
+        """
+        email = "test@test.com"
+        _pword = "test"
+        user = User.query.filter_by(email=email).first()
+        bucketlist_no = BucketList.query.filter_by(user_id=user.id).count()
+        response = self.add_bucketlist(email, _pword, user.id, 'test_bucketlist_name')
+        result = json.loads(response.data)
+        self.assertEqual(response.status, 201)
+        self.assertEqual(result['message'], 'Bucketlist Added')
+        new_bucketlist_no = BucketList.query.filter_by(user_id=user.id).count()
+        self.assertNotEqual(bucketlist_no, new_bucketlist_no)
+
+
     def add_bucketlist(self, email, password, user_id, buckelist_name):
         """
         Method is used to send request to the api to add a bucketlist for testing
