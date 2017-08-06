19092b98957c653dec2f37b7eca38f8a54a72098
Male Michael <malmike21@outlook.com>
2017-08-06 12:24:07 +0300

    Added method to handle post request to add bucketlist

--- myapp/api/bucketlist_endpoints/views.py
+++ myapp/api/bucketlist_endpoints/views.py
@@ -1,7 +1,5 @@
 """
-This file contains the endpoints for authentication
-i.e user registration, user authentication, user verification
-and logout
+This file contains the endpoints for bucketlists
 """
 from flask_restplus import Namespace, Resource, abort, fields
 from flask import request, g
@@ -12,23 +10,53 @@ from myapp.utilities.Utilities import auth
 bucketlist_api = Namespace('bucketlist', description='Bucketlist Details')
 
 BUCKETLIST = bucketlist_api.model(
-    'bucketlist',
+    'Bucketlist',
     {
         'name': fields.String(
             required=True,
             description="Bucketlist Name",
             example="test_bucketlist"),
-        'date_created': fields.DateTime(attribute='created'),
-        'date_modified': fields.DateTime(attribute='modified'),
-        'created_by':fields.Integer(attribute='user_id')
+        'date_created': fields.DateTime(required=False, attribute='created'),
+        'date_modified': fields.DateTime(required=False, attribute='modified'),
+        'created_by':fields.Integer(required=True, attribute='user_id')
     }
 )
 
 
-@auth.login_required
-@bucketlist_api.route('/bucketlist', endpoint='bucketlist')
+@bucketlist_api.route('', endpoint='bucketlist')
 class BucketListEndPoint(Resource):
     """
     Class contains operations which handle requests specific to a bucketlist
     """
-    pass
+    @auth.login_required
+    @bucketlist_api.response(201, 'Successful Bucketlist Added')
+    @bucketlist_api.response(409, 'Bucketlist Exists')
+    @bucketlist_api.response(
+        500,
+        'Server encountered an unexpected condition that prevented it from fulfilling the request.'
+    )
+    @bucketlist_api.doc(model='Bucketlist', body=BUCKETLIST)
+    def post(self):
+        """
+        Handles adding of new bucketlists
+        """
+        post_data = request.get_json()
+        name = post_data.get('name')
+        created_by = post_data.get('created_by')
+
+        bucketlist = BucketList(name=name, user_id=created_by)
+
+        try:
+            check = bucketlist.add_bucketlist()
+            if check:
+                response = {
+                    'status': 'success',
+                    'message': 'Bucketlist Added'
+                }
+                return response, 201
+            else:
+                response = {'status': 'fail', 'message': 'Bucketlist Exists'}
+                return response, 409
+        except Exception as e:
+            return abort(500, message='Error creating your account:{}'.format(e.message))
+
