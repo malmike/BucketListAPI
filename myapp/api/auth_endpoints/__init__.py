bfbb22580016abf3746f3482fa246e00f4494d09
Male Michael <malmike21@outlook.com>
2017-08-06 10:18:14 +0300

    Moved auth_endpoint blueprint and flask_restplus api initialisation to the init file in myapp.api module

--- myapp/api/auth_endpoints/__init__.py
+++ myapp/api/auth_endpoints/__init__.py
@@ -1,13 +1,1 @@
-"""
-Initializes the auth endpoint module with blueprints
-"""
-from flask import Blueprint
-from flask_restplus import Api
-from .views import auth_api
 
-auth_blueprint = Blueprint('auth', __name__)
-api_v1 = Api(auth_blueprint)
-
-# Set up paths for the auth endpoints
-##User registration path
-api_v1.add_namespace(auth_api)
