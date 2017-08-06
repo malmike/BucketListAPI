faac5f1bb20f8b1321aac15c6339553a0e1e557f
Male Michael <malmike21@outlook.com>
2017-08-06 12:06:24 +0300

    Added method for adding test bucketlist

--- tests/api/test_bucketlist_endpoints.py
+++ tests/api/test_bucketlist_endpoints.py
@@ -5,9 +5,22 @@ from unittest import TestCase
 import json
 
 from tests.base_case import BaseCase
+from myapp.models.user import User
+from myapp.models.bucketlist import BucketList
 
 class BucketlistEndPointsTests(BaseCase, TestCase):
     """
     Class contains tests for the bucketlist endpoints
     """
-    pass
+    def add_bucketlist(self, email, password, user_id, buckelist_name):
+        """
+        Method is used to send request to the api to add a bucketlist for testing
+        """
+        headers = self.authentication_headers(email=email, password=password)
+        return self.client.post(
+            '/api/v1/bucketlist',
+            data=json.dumps({"name": buckelist_name, "created_by": user_id}),
+            content_type="application/json",
+            headers=headers,
+            follow_redirects=True
+        )
