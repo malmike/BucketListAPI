aebb32fe77c6e7648a6e4fe6058aa8947dbac934
Male Michael <malmike21@outlook.com>
2017-08-07 16:09:25 +0300

    Added file that will contain tests for bucketlist item endpoints

---
+++ tests/api/test_bucketlist_item_endpoints.py
@@ -0,0 +1,15 @@
+"""
+File contains tests for the bucketlist item endpoints
+"""
+from unittest import TestCase
+import json
+
+from tests.base_case import BaseCase
+from myapp.models.user import User
+from myapp.models.bucketlist import BucketList
+
+class BucketlistEndPointsTests(BaseCase, TestCase):
+    """
+    Class contains tests for the bucketlist endpoints
+    """
+    pass
