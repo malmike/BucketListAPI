47b6d3f5902ff23d39c603f02654ca08fdf8a013
Male Michael <malmike21@outlook.com>
2017-08-07 16:07:18 +0300

    Added bucketlist_item_api namespace to the restplus api api_v1

--- myapp/api/__init__.py
+++ myapp/api/__init__.py
@@ -2,6 +2,7 @@ from flask import Blueprint
 from flask_restplus import Api
 from .auth_endpoints.views import auth_api
 from .bucketlist_endpoints.views import bucketlist_api
+from .bucketlist_item_endpoints.views import bucketlist_item_api
 
 api_v1_blueprint = Blueprint('api', __name__)
 
@@ -9,3 +10,4 @@ api_v1 = Api(api_v1_blueprint)
 
 api_v1.add_namespace(auth_api)
 api_v1.add_namespace(bucketlist_api)
+api_v1.add_namespace(bucketlist_item_api)
