cb46c82b7b6d8b9453363d3289e21d6322a62a5f
Male Michael <malmike21@outlook.com>
2017-08-07 16:37:04 +0300

    Added endpoint used for retrieveing bucketlist items

--- myapp/api/bucketlist_item_endpoints/views.py
+++ myapp/api/bucketlist_item_endpoints/views.py
@@ -3,10 +3,11 @@ This file contains the endpoints for bucketlists items
 """
 from flask_restplus import Namespace, Resource, abort, fields
 from flask import request, g
+from myapp.models.bucketlist import BucketList
 from myapp.models.bucketlist_item import BucketListItem
 from myapp.utilities.Utilities import auth
 
-bucketlist_item_api = Namespace('bucketlist_item', description='Bucketlist Items Details')
+bucketlist_item_api = Namespace('bucketlist', description='Bucketlist And Bucketlist Items')
 
 BUCKETLISTITEM = bucketlist_item_api.model(
     'BucketlistItem',
@@ -28,3 +29,20 @@ class BucketListItemEndPoint(Resource):
     """
     Class contains operations which handle requests specific to a bucketlist items
     """
+    @bucketlist_item_api.header('x-access-token', 'Access Token', required=True)
+    @auth.login_required
+    @bucketlist_item_api.response(200, 'Successful Retreival of bucketlist items')
+    @bucketlist_item_api.response(400, 'No existing bucketlist with the id passes')
+    @bucketlist_item_api.marshal_with(BUCKETLISTITEM, as_list=True)
+    def get(self, bucketlist_id):
+        """
+        Handles the retreival of a bucketlist's items
+        """
+        bucketlist = BucketList.query.filter_by(user_id=g.current_user.id, id=bucketlist_id).first()
+        if bucketlist:
+            return bucketlist.bucketlist_items
+        abort(400, 'Bucketlist with ID {} not found in the database'.format(bucketlist_id))
+
+    
+
+
