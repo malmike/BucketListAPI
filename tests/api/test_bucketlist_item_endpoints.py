5965f600aa6a33f0219d39f05f7cb260142f4b49
Male Michael <malmike21@outlook.com>
2017-08-07 16:36:16 +0300

    Added test for getting bucketlist items

--- tests/api/test_bucketlist_item_endpoints.py
+++ tests/api/test_bucketlist_item_endpoints.py
@@ -12,4 +12,23 @@ class BucketlistEndPointsTests(BaseCase, TestCase):
     """
     Class contains tests for the bucketlist endpoints
     """
-    pass
+    def test_get_bucketlist_items(self):
+        """
+        Method test the endpoint for getting bucketlists
+        For the user we will login using an existing user email:'test@test.com', password: 'test'
+        We will check the number of items brought back to those when directly accessing the database
+        """
+        email = "test@test.com"
+        _pword = "test"
+        user = User.query.filter_by(email=email).first()
+        bucketlist = BucketList.query.filter_by(user_id=user.id, id=1).first()
+        items_no = len(bucketlist.bucketlist_items)
+        headers = self.authentication_headers(email=email, password=_pword)
+        response = self.client.get(
+            '/api/v1/bucketlist/1/items/',
+            content_type="application/json",
+            headers=headers,
+            follow_redirects=True
+        )
+        result = json.loads(response.data)
+        self.assertEqual(len(result), items_no)
