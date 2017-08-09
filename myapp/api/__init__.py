701b9754d8bf3495d771b0247b8c8c056be5977c
Male Michael <malmike21@outlook.com>
2017-08-09 23:52:26 +0300

    Implemented CORS on the api_v1 blueprint module

--- myapp/api/__init__.py
+++ myapp/api/__init__.py
@@ -1,10 +1,12 @@
 from flask import Blueprint
 from flask_restplus import Api
+from flask_cors import CORS
 from .auth_endpoints.views import auth_api
 from .bucketlist_endpoints.views import bucketlist_api
 from .bucketlist_item_endpoints.views import bucketlist_item_api
 
 api_v1_blueprint = Blueprint('api', __name__)
+CORS(api_v1_blueprint)
 
 api_v1 = Api(api_v1_blueprint)
 
